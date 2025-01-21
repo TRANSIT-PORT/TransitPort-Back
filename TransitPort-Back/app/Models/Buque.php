e6de8410c2e059760e69902fd33318a9fc2d291e
julia <julia.g.avila@gmail.com>
2025-01-21 13:15:39 +0100

    AlgunosModelos

---
+++ TransitPort-Back/app/Models/Buque.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Administrativo;
+
+class Buque extends Model
+{
+    protected $table = 'buque';
+    protected $primaryKey = 'id';
+    protected $fillable = ['amarre', 'procedencia', 'destino', 'id_administrativo'];
+
+    public function administrativos()
+    {
+        return $this->belongsToMany(Administrativo::class);
+    }
+}
---
+++ TransitPort-Back/app/Models/Zona.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Gestor;
+use App\Models\Patio;
+use App\Models\Grua;
+
+class Zona extends Model
+{
+    protected $table = 'zona';
+    protected $primaryKey = 'id';
+    protected $fillable = ['ubicacion', 'capacidad', 'id_gestor', 'id_patio', 'id_grua'];
+
+    public function gestores()
+    {
+        return $this->belongsToMany(Gestor::class);
+    }
+    public function patios()
+    {
+        return $this->belongsToMany(Patio::class);
+    }
+    public function gruas()
+    {
+        return $this->belongsToMany(Grua::class);
+    }
+}
