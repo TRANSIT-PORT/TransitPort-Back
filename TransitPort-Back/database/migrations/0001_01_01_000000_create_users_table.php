5b70e7eb896235bbb153f6728d8aa432410d5d38
julia <julia.g.avila@gmail.com>
2025-02-14 13:29:36 +0100

    13:29

--- TransitPort-Back/database/migrations/0001_01_01_000000_create_users_table.php
+++ TransitPort-Back/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('estado');
             $table->string('usuario')->unique();
+            $table->string('ciudad');
+            $table->string('telefono');
+            $table->string('codigoPostal');
             $table->string('password');
             $table->enum('cargo', ['Gestor', 'Administrativo', 'Operador']);
             $table->rememberToken();
