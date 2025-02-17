39d0abcdc015a76c7e400e7b1e13fd6ca439017d
julia <julia.g.avila@gmail.com>
2025-02-17 08:42:04 +0100

    Los middleware básicos añadidos

--- TransitPort-Back/bootstrap/app.php
+++ TransitPort-Back/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\GestorMiddleware;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'gestor' => GestorMiddleware::class,
+            'administrativo' => GestorMiddleware::class,
+            'operador' => GestorMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
