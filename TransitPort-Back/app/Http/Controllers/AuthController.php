480ffe03d8908619be731f14dae3eb9067070d69
Alberto Zamora <spikejrgd@gmail.com>
2025-02-25 19:38:12 +0100

    Avances del Login, supongo

---
+++ TransitPort-Back/app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+
+class UserController extends Controller {
+
+    public $successStatus = 200;
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login() {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+
+            return response()->json(['success' => $success], $this->successStatus)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+    }
+}
