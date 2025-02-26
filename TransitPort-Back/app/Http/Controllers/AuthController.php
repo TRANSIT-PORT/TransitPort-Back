61d953e0fa67c1db16d38e178fff6c2181974e6d
alberto <albzamlan@alu.edu.gva.es>
2025-02-26 10:01:44 +0100

    LogIn Angular con Cargo en el Menu

--- TransitPort-Back/app/Http/Controllers/AuthController.php
+++ TransitPort-Back/app/Http/Controllers/AuthController.php
@@ -22,8 +22,9 @@ class AuthController extends Controller
     {
         if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
             $user = Auth::user();
+            $cargo = $user -> cargo;
             $success['token'] =  $user->createToken('MyApp')->accessToken;
-            return response()->json(['success' => $success, 'user'=> $user],
+            return response()->json(['success' => $success, 'user'=> $user, 'cargo' => $cargo],
             $this->successStatus);
         } else {
             return response()->json(['error' => 'Unauthorised'], 401);
