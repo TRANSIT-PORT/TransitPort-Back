63956f482817ac8b1aefb1cadd583f3854065280
julia <julia.g.avila@gmail.com>
2025-02-26 10:15:29 +0100

    10:15

--- TransitPort-Back/routes/api.php
+++ TransitPort-Back/routes/api.php
@@ -11,10 +11,18 @@ use App\Http\Controllers\OrdenController;
 use App\Http\Controllers\TieneController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\OrdenesController;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+//Login y Logout
+
+Route::post('/login', [AuthController::class, 'login']);
+
+//
 
 Route::get('/usuario', [UsuarioController::class, 'index']);
 
